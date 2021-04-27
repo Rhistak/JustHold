5135d8197d1bb78892ff323a14581b7975813b45
Santiago Ordiz <santiagoordiz@hotmail.com>
2021-04-27 14:14:42 -0300

    notcrypto

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -97,6 +97,10 @@
         </div>
       </q-form>
     </transition-group>
+    <!-- NOTCRYPTOS -->
+    <div v-if="currencies.length === 0" class="q-pa-md" style="color:white">
+      <p>You have not added any crypto yet, press the plus symbol to start!</p>
+    </div>
     <!-- ITEM  HEADER -->
     <div class="separatorItems">
       <q-list v-for="s in someCurrencies" :key="s.id">
@@ -315,12 +319,41 @@
               <q-icon name="attach_money" />
             </template>
           </q-input>
+          <q-input
+            filled
+            v-model="date"
+            mask="date"
+            :rules="['date']"
+            bg-color="accent"
+            square
+            placeholder="Buy Date"
+          >
+            <template v-slot:append>
+              <q-icon name="event" class="cursor-pointer">
+                <q-popup-proxy
+                  ref="qDateProxy"
+                  transition-show="scale"
+                  transition-hide="scale"
+                >
+                  <q-date v-model="date" today-btn>
+                    <div class="row items-center justify-end">
+                      <q-btn v-close-popup label="Close" color="primary" flat />
+                    </div>
+                  </q-date>
+                </q-popup-proxy>
+              </q-icon>
+            </template>
+          </q-input>
         </div>
         <div class="btnDiv">
           <q-btn type="submit" label="add" class="right" icon="add_box" />
         </div>
       </q-form>
     </transition-group>
+    <!-- NOTCRYPTOS -->
+    <div v-if="currencies.length === 0" class="q-pa-md" style="color:white">
+      <p>You have not added any crypto yet, press the plus symbol to start!</p>
+    </div>
     <!-- $KELETON -->
     <div v-for="c in someCurrencies" :key="c.id" class="q-mb-md">
       <q-card-section style="margin:0;padding:0">
